3bf23b918474f119e1b81ef6859cae389fa21527
ml.maxofs2d@gmail.com <ml.maxofs2d@gmail.com@6d860ee7-4cb9-ddda-ac91-55a8d7aed2cf>
2013-06-01 16:04:02 +0000

    Got rid of more unused stuff

--- trunk/compile_sfm.bat
+++
@@ -1,9 +0,0 @@
-:COMPILE
-
-"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\SourceFilmmaker\game\usermod" "C:\Users\Max\Dropbox\Max\gmod_player\player_male_anims_m_anm.qc"
-"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\SourceFilmmaker\game\usermod" "C:\Users\Max\Dropbox\Max\gmod_player\player_female_anims_f_anm.qc"
-"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\SourceFilmmaker\game\usermod" "C:\Users\Max\Dropbox\Max\gmod_player\player_zombie_anims_z_anm.qc"
-
-PAUSE
-
-goto COMPILE
