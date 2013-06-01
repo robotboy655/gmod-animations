c8e2f121b584c1295b278ff5e3b70834ba8d39bc
ml.maxofs2d@gmail.com <ml.maxofs2d@gmail.com@6d860ee7-4cb9-ddda-ac91-55a8d7aed2cf>
2013-06-01 15:27:43 +0000

    Initial commit

---
+++ trunk/compile_mdl.bat
@@ -0,0 +1,7 @@
+:COMPILE
+
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\SourceFilmmaker\game\usermod" "C:\Users\Max\Dropbox\Max\hl2\models\Humans\Male\Group01\male_07_gmod.qc"
+
+PAUSE
+
+goto COMPILE
---
+++ trunk/compile_svn_release.bat
@@ -0,0 +1,9 @@
+:COMPILE
+
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_male_anims_m_anm.qc"
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_female_anims_f_anm.qc"
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_zombie_anims_z_anm.qc"
+
+PAUSE
+
+goto COMPILE
