b9612214b81e6ed5ff1ce7d1a0e0354e865e7a80
ml.maxofs2d@gmail.com <ml.maxofs2d@gmail.com@6d860ee7-4cb9-ddda-ac91-55a8d7aed2cf>
2013-06-01 16:33:43 +0000

    We don't need this either

--- compile_svn_release.bat
+++ compile_svn_release.bat
@@ -1,9 +1,21 @@
+@ECHO OFF
+ECHO Make sure to right click, run as administrator on this batch file.
+ECHO.
+@ECHO ON
 :COMPILE
 
 "%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_male_anims_m_anm.qc"
 "%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_female_anims_f_anm.qc"
 "%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garrysmod\garrysmod" "C:\Users\Max\Dropbox\SDK\Animation\gmod\player_zombie_anims_z_anm.qc"
 
+@ECHO OFF
+ECHO.
+ECHO.
+ECHO.
+ECHO Press any key to start compiling again.
+ECHO.
+
 PAUSE
 
+@ECHO ON
 goto COMPILE
