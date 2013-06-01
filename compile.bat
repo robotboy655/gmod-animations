58b8fa806ff1324cf7ce902fa2b5525f8eb33666
ml.maxofs2d@gmail.com <ml.maxofs2d@gmail.com@6d860ee7-4cb9-ddda-ac91-55a8d7aed2cf>
2013-06-01 15:27:43 +0000

    Initial commit

---
+++ compile.bat
@@ -0,0 +1,8 @@
+:COMPILE
+
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garry's mod beta\garrysmodbeta" "C:\Users\Max\Dropbox\Max\gmod_player\player_male_anims_m_anm.qc"
+"%sourcesdk%\bin\orangebox\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\steamapps\max_of_s2d\garry's mod beta\garrysmodbeta" "C:\Users\Max\Dropbox\Max\gmod_player\player_female_anims_f_anm.qc"
+
+PAUSE
+
+goto COMPILE
